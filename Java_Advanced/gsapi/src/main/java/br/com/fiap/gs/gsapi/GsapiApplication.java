e8e50f13b1e30662f81d2d230cc6effb50d7f600
carmipa <rm557881@fiap.com.br>
2025-05-26 21:13:25 -0300

    java projeto e bd modelagem inicial!

---
+++ Java_Advanced/gsapi/src/main/java/br/com/fiap/gs/gsapi/GsapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gs.gsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsapiApplication.class, args);
+	}
+
+}
---
+++ Java_Advanced/gsapi/src/test/java/br/com/fiap/gs/gsapi/GsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gs.gsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
